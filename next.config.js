2b4691b1857a77ccc0ae4ce24139825f4145cf45
EmmanuelN1 <emmanuelnzeakor00@yahoo.com>
2025-04-17 13:32:13 +0100

    `Removed next.config.ts and updated tsconfig.json to include next.config.js`

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+};
+
+module.exports = nextConfig;
