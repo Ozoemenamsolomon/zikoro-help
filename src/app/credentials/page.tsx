cf73483ffd670c5b96d3b2fc4e057032caff63b2
EmmanuelN1 <emmanuelnzeakor00@yahoo.com>
2025-04-23 20:17:36 +0100

    fix: updated cred  page

--- src/app/credentials/page.tsx
+++ src/app/credentials/page.tsx
@@ -1,3 +1,9 @@
+import CredBody from "@/components/cred/CredBody";
+import HeroCred from "@/components/cred/HeroCred";
+
 export default function Credentials() {
-  return <div></div>;
+  return <div>
+    <HeroCred/>
+    <CredBody/>
+  </div>;
 }
